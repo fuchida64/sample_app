d6e1fe76ba9833d0a221f315687b7127610596e7
fuchida64 <crbsfy@icloud.com>
2018-06-14 02:21:48 +0900

    Finish 3-6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,18 @@ class PostsController < ApplicationController
   def new
   	 @post = Post.new
   end
+  def show
+  	@post = Post.find(params[:id])
+  end
   def create
   	post = Post.new(post_params)
   	post.save
-  	redirect_to '/top'
+  	redirect_to post_path(post.id)
   end
 
+  def index
+  	@posts = Post.all
+  end
 
   private
 
