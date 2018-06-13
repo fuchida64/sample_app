e814812149bd6ea3801f2e9727f1fc31456d016e
fuchida64 <crbsfy@icloud.com>
2018-06-14 06:28:12 +0900

    Finish 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
 	 get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
 	 patch '/posts/:id' => 'posts#update', as: 'update_post'
 end
+Refile.secret_key = '8877b9eb41d2e71abd34f0487a7b87b7dc1303a65eb35b5c15a3d533c2987584f46da93d93f0fb6410b2ed77afe5fc76c1cc08421c7e15ccd99bc7db844e770a'
