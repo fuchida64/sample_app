4d6e0183b9ef84942a861a15b65202c6c6a1ea3a
fuchida64 <crbsfy@icloud.com>
2018-06-14 03:36:19 +0900

    Finish 3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,14 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
+  def edit
+        @post = Post.find(params[:id])
+  end
+  def update
+  		post = Post.find(params[:id])
+        post.update(post_params)
+        redirect_to post_path(post.id)
+  end
 
   private
 
